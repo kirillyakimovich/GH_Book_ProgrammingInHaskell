54603bfe15aab2ee6e393738282e554ca6a10923
Kirill Yakimovich <Kirill.Yakimovich@heidelberg-mobil.com>
2016-01-29 23:36:49 +0300

    [CH5] exercises finished

--- Ch5.hsproj/Caesar.hs
+++ Ch5.hsproj/Caesar.hs
@@ -9,6 +9,9 @@ positions x xs = [i | (x', i) <- zip xs [0..n], x == x']
 lowers :: String -> Int
 lowers xs = length [x | x <- xs, isLower x]
 
+letters :: String -> Int
+letters xs = length [x | x <- xs, isLetter x]
+
 count :: Char -> String -> Int
 count x xs = length [x' | x' <- xs, x == x']
   
@@ -17,9 +20,16 @@ let2int c = ord c - ord 'a'
 
 int2let :: Int -> Char
 int2let n = chr(ord 'a' + n)
+
+cap2int :: Char -> Int
+cap2int c = ord c - ord 'A'
+
+int2cap :: Int -> Char
+int2cap n = chr(ord 'A' + n)
  
 shift :: Int -> Char -> Char
 shift n c | isLower c = int2let((let2int c + n) `mod` 26)
+          | isUpper c = int2cap((cap2int c + n) `mod` 26)
           | otherwise = c
           
 encode :: Int -> String -> String
@@ -32,8 +42,10 @@ percent :: Int -> Int -> Float
 percent n m = (fromIntegral n / fromIntegral m) * 100
 
 freqs :: String -> [Float]
-freqs xs = [percent (count x xs) n | x <- ['a'..'z']]
-            where n = lowers xs
+freqs xs = [percent (count x lowerCased) n | x <- ['a'..'z']]
+            where
+               n = letters xs
+               lowerCased = [toLower x | x <- xs]
             
 chisqr :: [Float] -> [Float] -> Float
 chisqr os es = sum [((o - e) ^ 2) / e | (o, e) <- zip os es]
--- Ch5.hsproj/Exercises.hs
+++ Ch5.hsproj/Exercises.hs
@@ -12,3 +12,12 @@ factors n = [x | x <- [1..n], n `mod` x == 0]
 perfects :: Int -> [Int]
 perfects n = [x | x <- [1..n], isPerfect x]
   where isPerfect n = (sum (init (factors n)) == n)
+  
+find :: Eq a => a -> [(a, b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
+
+positions :: Eq a => a -> [a] -> [Int]
+positions a xs = find a (zip xs [1..])
+
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [x * y | (x, y) <- zip xs ys]
