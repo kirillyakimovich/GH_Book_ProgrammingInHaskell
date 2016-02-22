8b9398c0275fd926a6638ed467deb975de4d494a
Kirill Yakimovich <Kirill.Yakimovich@heidelberg-mobil.com>
2016-02-22 23:33:27 +0300

    [UPD] Chapter 7 exercises finished

--- PiH.hsproj/Chapter7_Higher_order_functions/Ex8.hs
+++ PiH.hsproj/Chapter7_Higher_order_functions/Ex8.hs
@@ -1,24 +1,32 @@
 module Chapter7_Higher_order_functions.Ex8 where
   
-import Chapter7_Higher_order_functions.String_transmitter
+import Chapter7_Higher_order_functions.String_transmitter hiding (encode, decode)
 import Chapter7_Higher_order_functions.Exercises
 import Data.Char
 
+parityBit :: [Bit] -> Bit
+parityBit xs = sum xs `mod` 2
+
 parity :: [Bit] -> [Bit]
-parity xs = xs ++ [mod (sum xs) 2]
+parity xs = xs ++ [parityBit xs]
 
---checkparity    :: [Bit] -> [Bit]
-
-
-encodep :: String -> [Bit]
-encodep = concat . map (parity . make8 . int2bin . ord)
+encode :: String -> [Bit]
+encode = concat . map (parity . make8 . int2bin . ord)
 
 chop      :: Int -> [a] -> [[a]]
 chop _ [] = []
 chop n xs = take n xs : chop n (drop n xs)
 
-decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+checkParity xs | parityBit (init xs) == last xs = init xs
+               | otherwise = error "Does not match"
 
---decodep :: [Bit] -> String
-  
+decode :: [Bit] -> String
+decode = map (chr . bin2int . checkParity ). (chop 9)
+
+faultyChannel :: [Bit] -> [Bit]
+faultyChannel = tail
+
+faultyTransmit :: String -> String
+faultyTransmit = decode . faultyChannel . encode
+
+
